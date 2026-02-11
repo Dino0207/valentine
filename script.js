24514dab10807fb2b0cc94b8263bdf5854af18b9
Dino0207 <deanlouise556@gmail.com>
2026-02-11 14:37:52 +0800

    Update script.js

--- script.js
+++ script.js
@@ -59,7 +59,7 @@ window.addEventListener('DOMContentLoaded', () => {
     validateConfig();
 
     // Set texts from config
-    //document.getElementById('valentineTitle').textContent = `${config.valentineName}, my love...`;
+    document.getElementById('valentineTitle').textContent = `YOU SAID YES!!!`;
     
     // Set first question texts
     document.getElementById('question1Text').textContent = config.questions.first.text;
@@ -314,3 +314,4 @@ function openLetter() {
     }
 
 }
+
